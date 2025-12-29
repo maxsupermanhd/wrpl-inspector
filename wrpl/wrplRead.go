2c774c00f6da09278fd016491d0605cfcb81d7a2
maxsupermanhd <q3.max.2011@ya.ru>
2025-12-29 21:00:27 +0300

    erm

--- wrpl/wrplRead.go
+++ wrpl/wrplRead.go
@@ -52,7 +52,7 @@ type ReplayReader struct {
 
 func (rpl *ReplayReader) Close() error {
 	if rpl.PacketStream != nil {
-		return rpl.Close()
+		return rpl.PacketStream.Close()
 	}
 	return nil
 }
@@ -168,7 +168,7 @@ func OpenReplay(r io.ReadSeeker, readSettings, openPackets, readResults bool) (r
 	}
 
 	if openPackets {
-		_, err := r.Seek(int64(ret.Header.SettingsBLKSize)+1232, io.SeekStart)
+		_, err := r.Seek(int64(ret.Header.SettingsBLKSize)+1226, io.SeekStart)
 		if err != nil {
 			return ret, fmt.Errorf("seeking for packets")
 		}
